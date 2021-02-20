b05d5c5d1af88249e76391775ce58e6e6c12b36e
Quentin Stoeckel <stoeckel.quentin@gmail.com>
2021-02-20 13:25:27 +0100

    Mdi.vue: change indentation from 4 to 2 spaces

--- src/Mdi.vue
+++ src/Mdi.vue
@@ -1,42 +1,42 @@
 <template>
-    <svg v-bind="allAttrs">
-        <path :d="d"></path>
-    </svg>
+  <svg v-bind="allAttrs">
+    <path :d="d"></path>
+  </svg>
 </template>
 
 <script>
-    const PATH_REGEX = /<path d="(.+)" \/>/;
+  const PATH_REGEX = /<path d="(.+)" \/>/;
 
-    export default {
-        name: 'mdi',
-        props: {
-            mdi: {
-                type: String,
-                required: true,
-            },
-            size: {
-                type: Number,
-                required: false,
-                default: 24,
-            },
-        },
-        computed: {
-            d() {
-                return PATH_REGEX.exec(this.mdi)[1];
-            },
-            allAttrs() {
-                return Object.assign(
-                    {},
-                    {
-                        viewBox: '0 0 24 24',
-                        xmlns: 'http://www.w3.org/2000/svg',
-                        width: this.size,
-                        height: this.size,
-                        role: 'presentation',
-                        class: this.className,
-                    },
-                );
-            }
-        }
-    };
+  export default {
+    name: 'mdi',
+    props: {
+      mdi: {
+        type: String,
+        required: true,
+      },
+      size: {
+        type: Number,
+        required: false,
+        default: 24,
+      },
+    },
+    computed: {
+      d() {
+        return PATH_REGEX.exec(this.mdi)[1];
+      },
+      allAttrs() {
+        return Object.assign(
+          {},
+          {
+            viewBox: '0 0 24 24',
+            xmlns: 'http://www.w3.org/2000/svg',
+            width: this.size,
+            height: this.size,
+            role: 'presentation',
+            class: this.className,
+          },
+        );
+      }
+    }
+  };
 </script>
